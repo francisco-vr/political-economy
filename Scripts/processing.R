6b547b7b2788c820e903a515a5b11351393bd94b
francisco-vr <gnusuri@debian-BULLSEYE-live-builder-AMD64>
2022-12-06 17:55:48 -0300

    update

--- Scripts/processing.R
+++ Scripts/processing.R
@@ -2,12 +2,13 @@
 ## Processing
 
 
-ipak <- fcomisionction(pkg){
+ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
     install.packages(new.pkg, dependencies = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
+
 
 # usage
 
@@ -76,7 +77,7 @@ X1 <- result$legislators$coord1D   # primera dimension
 X2 <- (result$legislators$coord2D)*WEIGHT # segcomisionda dimension
 windows()
 
-plot(X1,X2,type="n",asp=1,
+plot(X1,type="n",asp=1,
      xlab="1a dimension",
      ylab="2a dimension",
      xlim=c(-1.0,1.0),ylim=c(-1.0,1.0),font=2,cex=1.2)
@@ -87,7 +88,7 @@ points(X1[result$legislators$party == "WP"],X2[result$legislators$party == "WP"]
 # el paquete trae por defecto la posibilidad de comision plot de los resultados
 plot(result)
 
-hola <-ggplot(result$legislators)
+table(result$beta)
 
 
 # tambien se puede separar para efectos de publicacion
@@ -96,105 +97,4 @@ plot.scree(result) # peso relativo de cada eigenvalue
 plot.cutlines(result, lines=5) # pcomisiontos de corte de cada proyecto
 plot.angles(result) # histograma de los pcomisiontos de corte por angulos
 
-# entonces ahora hagamos lo mismo con datos de Chile
-senado <- read.csv(paste0(aqui,"/votos2014_2016_procesado.csv"),sep=";")
-nombres <- senado[,1]
-senado <- senado[,2:NCOL(senado)]
-rc_senado <- rollcall(senado,             
-               yea=c(1), # reduce los valores a dos grupos yea/nay
-               nay=c(-1),
-               missing=c(0), # todos los otros datos quedan como missing
-               notInLegis=NULL, # vector de ausentes en que seccion
-               legis.names=nombres,
-               legis.data=NULL,
-               desc="Senado 2014-15")
-result_senado <- wnominate(rc_senado, dims=2, polarity=c(35,35))
-summary(result_senado) # el objeto results contiene la estimacion
-windows()
-plot(result_senado)
 
-WEIGHT=(result_senado$weights[2])/(result_senado$weights[1]) # peso relativo dado a la segcomisionda dimension
-X1 <- result_senado$legislators$coord1D   # primera dimension
-X2 <- (result_senado$legislators$coord2D)*WEIGHT # segcomisionda dimension
-
-
-# Veamos el grafico ahora paso a paso
-
-proyecto <- 27
-voto <- as.integer(rc_senado$votes[,proyecto])
-DL1 <- result_senado$rollcalls[proyecto,7]  # spread en dim 1
-DL2 <- result_senado$rollcalls[proyecto,8]  # spread en dim 2
-ZM1 <- result_senado$rollcalls[proyecto,9]  # pcomisionto medio en dim 1
-ZM2 <- result_senado$rollcalls[proyecto,10] # pcomisionto medio en dim 2
-YEA1 <- ZM1-DL1              # diff entre pcomisionto medio y desv en dim 1
-YEA2W <- (ZM2-DL2)*WEIGHT    # diff entre pcomisionto medio y desv en dim 2
-NAY1 <- ZM1+DL1              # idem para votos no en dim 1
-NAY2W <- (ZM2+DL2)*WEIGHT    # idem para votos no en dim 2
-A1 <- NAY1 - YEA1            # distancia yes-no  
-A2 <- NAY2W - YEA2W
-ALENGTH <- sqrt(A1*A1 + A2*A2) # radio
-N1W <- A1/ALENGTH            # distancia (yes-no)/radio
-N2W <- A2/ALENGTH
-if (N1W < 0){  # nos aseguramos que sean distancias, por lo tanto, signo +
-  N1W <- -N1W
-  N2W <- -N2W
-}
-ws <- N1W*ZM1 + N2W*ZM2*WEIGHT  # pondera pesos
-xws <- ws*N1W
-yws <- ws*N2W
-
-
-windows()
-plot(X1, X2,
-     xlab="1ra dimensión (izquierda-derecha)
-     N=En contra, S=A favor",
-     ylab="2da Dimensión",
-     #col=partidos,
-     pch=16,
-     main="Senado - WNOMINATE - Proyecto de ley")
-segments(xws+N2W, yws-N1W, xws-N2W, yws+N1W, col="black", lwd=2)
-
-# Boostrapping en WNOMINATE
-library(ellipse)
-result_senado_boot <- wnominate(rc_senado, 
-                                   ubeta=15, # valores por defecto
-                                   uweights=0.5, # valores por defecto
-                                   dims=2, 
-                                   minvotes=10,
-                                   lop=0.025, 
-                                   trials=20, # boots
-                                   polarity=c(1,5), 
-                                   verbose=FALSE)
-std1 <- result_senado_boot$legislators$se1D
-std2 <- result_senado_boot$legislators$se2D * WEIGHT
-corr12 <- result_senado_boot$legislators$corr.1
-
-windows()
-plot(X1, X2,
-     xlab="1ra dimensiÃ³n (izquierda-derecha)
-     Pcomisiontos: Ideal points; Elipses: Margen de error",
-     ylab="2da DimensiÃ³n",
-     #col=partidos,
-     pch=16,
-     main="Ejemplo - WNOMINATE
-     Intervalos de confianza de pcomisiontos ideales - NM",
-     type="n")
-
-for (i in 1:nrow(result_senado_boot$legislators)){
-#  if(!is.na(corr12[i]) & (coaliciones[i]=="red")){
-  if(!is.na(corr12[i]) ){  
-    lines(c(X1[i],X1[i]), c(X2[i]- 1.96*std2[i], X2[i] + 1.96*std2[i]),col="gray")
-    lines(c(X1[i] - 1.96*std1[i], X1[i] + 1.96*std1[i]), c(X2[i], X2[i]),col="gray")
-    if (abs(corr12[i]) > .30){
-      lines(ellipse(x=corr12[i], scale=c(std1[i],std2[i]),centre=c (X1[i] ,X2[i])), col="gray")
-    }
-  }
-}
-#points(X1[coaliciones == "red"], X2[coaliciones == "red"], pch=16, col="red", font=2)
-segments(xws+N2W, yws-N1W, xws-N2W, yws+N1W, lwd=2, col="black")
-polarity <- X1*N1W + X2*N2W - ws
-
-
-library(devtools)
-devtools::install_github("jaytimm/wnomadds")
-library(wnomadds)
